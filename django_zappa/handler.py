1e763f6271b1d16dbb5284b8adae72cf9885f393
Aneesh Kumar <anush0247@gmail.com>
2016-12-10 14:18:18 +0530

    added exception handling

--- django_zappa/handler.py
+++ django_zappa/handler.py
@@ -84,7 +84,10 @@ def lambda_handler(event, context, settings_name="zappa_settings"):  # NoQA
                 
         os_env_to_remove = ["AWS_SESSION_TOKEN", "AWS_SECURITY_TOKEN"]
         for each_key in os_env_to_remove:
-            del os.environ[each_key]
+            try:
+                del os.environ[each_key]
+            except KeyError:
+                pass
     except:
         logger.error("Error in stage_vars")
     # If in DEBUG mode, log all raw incoming events.
